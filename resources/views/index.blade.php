a6789a4721347df60734f8f481b1fa28d13a84fc
GuljahanG <gurbanmyradownaguljahan@gmail.com>
2026-06-22 00:31:26 +0500

    updated folder icons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,20 +40,25 @@
 
     <div class="group">
 
-        <button class="group-title">
+        <span class="group-title">
+            <img
+                class="folder"
+                width="22"
+                height="22"
+                src="{{ asset('vendor/swaglite/folder.png') }}"
+            >
 
-            <span>
-                📁 {{ $group ?: 'General' }}
-                ({{ $items->count() }})
-            </span>
-
-            <span class="group-arrow">
-                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"/>
-                </svg>
-            </span>
-
-        </button>
+            <img
+                class="open-folder"
+                width="22"
+                height="22"
+                src="{{ asset('vendor/swaglite/open-folder.png') }}"
+            >
+            <div class="title">
+                {{ $group ?: 'General' }} <span> ({{ $items->count() }}) </span>
+            </div>
+ 
+        </span>
 
         <div class="group-body">
 
